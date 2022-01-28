8411944d1e0e6474f300c727377f70f005c22a9c
srioktavia21 <83336272+srioktavia21@users.noreply.github.com>
2022-01-28 16:18:53 +0700

    Add files via upload

---
+++ DestinationActivity.java
@@ -0,0 +1,15 @@
+package com.informatika19100073.srioktavia_19100073_alarm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
